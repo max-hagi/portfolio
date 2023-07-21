d9136cff1e260c45d3a914b852b14b1d184d7cc0
max-hagi <msavehilaghi99@gmail.com>
2023-07-20 21:57:21 -0400

    updated projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,41 @@
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import thing from "../assets/memoji.PNG"
 
+import "./Projects.css"
 
 export const Projects = () => {
     
     return (
         <section className="project" id="project"> 
             <div>
-
-                <h1>HI</h1>
-
+                <h1>Projects</h1>
+            </div>
+            
+            <div className="tabs">
+                <Tab.Container defaultActiveKey="first" fill="true" justify>
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column nav">  
+                                <Nav.Item className='nav-item'>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item className='nav-item'>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content className='tab-content'>
+                                <Tab.Pane eventKey="first"><img src={thing}/></Tab.Pane>
+                                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
             </div>
         </section>
     );
